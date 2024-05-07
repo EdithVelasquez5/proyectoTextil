c9eb79d3ec3154825379302291758b509f2eda55
Dant3f77 <daniel.bonillahdz@gmail.com>
2024-05-06 22:57:43 -0600

    se pagina inicio branch daniel

--- login/inicio.php
+++ login/inicio.php
@@ -1,20 +1,89 @@
-<?php 
+<?php
 include_once('../templates/header.php');
 
 session_start();
-
 include_once('../templates/navbar.php');
-
+$userName = $_SESSION['username'];
 
 // Verificar si el usuario está autenticado
 if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
-    echo "Bienvenido, " . $_SESSION['username'] . "!";
+    //echo "Bienvenido, " . $_SESSION['username'] . "!";
 } else {
     echo "No estás autorizado para ver esta página.";
 }
 ?>
-    <h2>Bienvenido, <?php echo $_SESSION['username']; ?>!</h2>
-    <p>Has iniciado sesión correctamente.</p>
-    <a href="logout.php"><i class="bi bi-apple  Heading" ></i>Cerrar sesión</a> <!-- Opción para cerrar sesión -->
+
+<div class="container">
+    <div class="row m-5">
+
+        <div class="col-4 ">
+            <div class="card text-center shadow " style="width: 15rem;">
+                <h4 class="card-title text-primary">Crear Pedido</h4>
+                <a href="">
+                <img src="../php/img/pedido.png" class="card-img-top" alt="...">
+                </a>
+            </div>
+        </div>
+
+       
+        <div class="col-4 ">
+            <div class="card text-center shadow " style="width: 15rem;">
+                <h4 class="card-title text-primary">Administrar Cliente</h4>
+                <a href="">
+                <img src="../php/img/administrarCliente.png" class="card-img-top" alt="...">
+                </a>
+            </div>
+        </div>
+
+        <div class="col-4 ">
+            <div class="card text-center shadow " style="width: 15rem;">
+                <h4 class="card-title text-primary">Administrar Pedido</h4>
+                <a href="">
+                <img src="../php/img/editarPedido.png" class="card-img-top" alt="...">
+                </a>
+            </div>
+        </div>
+
+
+    </div>
+<?php 
+if ($userName == "jefe" || $userName == "admin" )
+{
+?>
+    <div class="row m-5">
+
+        <div class="col-4 ">
+            <div class="card text-center shadow " style="width: 15rem;">
+                <h4 class="card-title text-primary">Graficos</h4>
+                <a href="">
+                <img src="../php/img/graficos.png" class="card-img-top" alt="...">
+                </a>
+            </div>
+        </div>
+        <div class="col-4 ">
+            <div class="card text-center shadow " style="width: 15rem;">
+                <h4 class="card-title text-primary">Generar Reportes</h4>
+                <a href="">
+                <img src="../php/img/reporte.png" class="card-img-top" alt="...">
+                </a>
+            </div>
+        </div>
+        <div class="col-4 ">
+            <div class="card text-center shadow " style="width: 15rem;">
+                <h4 class="card-title text-primary">Administrar Usuario</h4>
+                <a href="">
+                <img src="../php/img/administrarUsuario.png" class="card-img-top" alt="...">
+                </a>
+            </div>
+        </div>
+
+
+    </div>
+
+    <?php 
+        }
+    ?>
+</div>
+
 
 <?php include_once('../templates/footer.php');
--- templates/navbar.php
+++ templates/navbar.php
@@ -2,7 +2,8 @@
 
 <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Bienvenido  <?php echo $_SESSION['username']; ?></a>
-      
+    <!-- <a class="navbar-brand" href="#">Bienvenido  <?php echo $_SESSION['username']; ?></a>-->
+    <h3 class="navbar-brand""> Bienvenido  <b class="text-warning"> <?php echo $_SESSION['username']; ?> </b></h3>
+    <a href="logout.php" class="btn btn-danger">Cerrar sesión</a> <!-- Opción para cerrar sesión -->
   </div>
 </nav>
